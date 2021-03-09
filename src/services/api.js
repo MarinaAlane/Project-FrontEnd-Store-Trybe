be6b5af87b5f172f9807187863b6f188ad2f7f8d
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-03-09 19:11:39 -0300

    corrigindo lint req1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 export async function getCategories() {
-  const url = "https://api.mercadolibre.com/sites/MLB/categories";
-  return fetch(url).then(data => data.json());
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return fetch(url).then((data) => data.json());
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  const url = "https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}"
-  return fetch(url).then(data => data.json());
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  return fetch(url).then((data) => data.json());
 }
