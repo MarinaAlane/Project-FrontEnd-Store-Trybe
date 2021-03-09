007b09e485140d4f526738f2b706bedff40db297
Perycles floriano <perycles_reis@hotamil.com>
2021-03-09 17:37:09 -0300

    requisito 1 parte 2

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,9 @@ export async function getCategories() {
     .catch((err) => console.log(err));
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((response) => response.json())
+    .then((data) => data)
+    .catch((err) => console.log(err));
 }
