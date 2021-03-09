dcfed033faf687a20f54f93457866564e971cb3f
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-03-09 17:18:56 -0300

    requirement 02 - go to cry

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import Search from './Search';
 import './App.css';
 
 function App() {
   return (
     <div className="App-header">
-     <Search />
+      <Search />
     </div>
   );
 }
--- src/Search.jsx
+++ src/Search.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react"; 
+import React, { Component } from 'react';
 
-class Search extends Component{
+class Search extends Component {
   render() {
     return (
       <div data-testid="home-initial-message">
         <h1> Digite algum termo de pesquisa ou escolha uma categoria.</h1>
       </div>
-        
     );
   }
 }
