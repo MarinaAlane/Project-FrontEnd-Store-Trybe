23a4319074f0015b8bca51604c97c4ab84a4694d
BernardoFontes <bfontes.1997@gmail.com>
2021-03-09 18:28:47 -0300

    aaaa

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Carrinho from './Carrinho';
 
 class Home extends React.Component {
   render() {
@@ -10,9 +9,9 @@ class Home extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-        <button data-testid="shopping-cart-button" type="button">
-          <Link to="/components/carrinho" component={ Carrinho } />
-        </button>
+          <Link data-testid="shopping-cart-button" to="/components/carrinho">
+            <i class="fas fa-shopping-cart"></i>
+          </Link>
       </section>
     );
   }
