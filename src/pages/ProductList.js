13a5bc8652ed28a82493363099d3419807ac57c1
Thiago Granville <thiagogranville@gmail.com>
2021-03-09 19:09:38 -0300

    [FEAT][REQUIREMENT 04][CATEGORIES LIST]

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
+import * as Api from '../services/api';
 
 class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    const categoriesResponse = await Api.getCategories();
+
+    this.setState({ categories: categoriesResponse });
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
       <main>
-        <header>
+        <nav>
           <input type="text" />
-        </header>
+        </nav>
+        <aside>
+          <span>Categorias:</span>
+          {categories.map((category) => (
+            <div key={ category.id }>
+              <label
+                data-testid="category"
+                htmlFor={ `${category.name}-checkbox` }
+              >
+                <input id={ `${category.name}-checkbox` } type="checkbox" />
+                { category.name }
+              </label>
+            </div>
+          ))}
+        </aside>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
