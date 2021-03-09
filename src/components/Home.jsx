e5ac1497ef551ffb37a8bd38d5d0bcd9d8b5a708
Caio <caioqs.info@gmail.com>
2021-03-09 19:18:32 -0300

    add/Requirement4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as api from '../services/api';
+import ListCategories from './ListCategories';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      promisse: false,
+      categories: '',
+    };
     this.getCategories = this.getCategories.bind(this);
     this.getProducts = this.getProducts.bind(this);
+    this.renderElements = this.renderElements.bind(this);
   }
 
   componentDidMount() {
@@ -15,14 +21,20 @@ class Home extends React.Component {
   }
 
   async getCategories() {
-    await api.getCategories();
+    const categories = await api.getCategories().then((data) => this.setState({
+      categories: data,
+      promisse: true,
+    }));
+    console.log(categories);
+    return categories;
   }
 
   async getProducts(categoryId, query) {
     await api.getProductsFromCategoryAndQuery(categoryId, query);
   }
 
-  render() {
+  renderElements() {
+    const { categories } = this.state;
     return (
       <div>
         <input type="text" />
@@ -34,7 +46,20 @@ class Home extends React.Component {
             Cart
           </Link>
         </button>
+        <div>
+          <ListCategories categories={ categories } />
+        </div>
       </div>
+    );
+  }
+
+  render() {
+    const { promisse } = this.state;
+    if (promisse === true) {
+      return this.renderElements();
+    }
+    return (
+      <div>Loading...</div>
     );
   }
 }
