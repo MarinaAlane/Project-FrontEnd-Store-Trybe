4b6064370cbb753552ce5d0b891007d24a18975d
OPedroAzevedo <pvas1996@gmail.com>
2021-03-09 16:41:27 -0300

    CategoryList

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as api from '../services/api';
+
+class CategoryList extends React.Component {
+  constructor() {
+    super();
+    this.updateState = this.updateState.bind(this);
+    this.state = {
+      category: [],
+    };
+  }
+
+  async componentDidMount() {
+    const category = await api.getCategories();
+    this.updateState(category);
+  }
+
+  updateState(category) {
+    this.setState({ category });
+  }
+
+  render() {
+    const { category } = this.state;
+    return (
+      <div>
+        {category.map((item) => (
+          <li
+            data-testid="category"
+            key={ item.id }
+          >
+            {item.name}
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CategoryList;
