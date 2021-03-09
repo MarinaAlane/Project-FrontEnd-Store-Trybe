44b334f90124de43336401cf5f79af5d63a2f908
RodrigoNeoEx <draja.acc@gmail.com>
2021-03-09 10:10:23 -0300

    created branch to align lint/climate

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 export async function getCategories() {
-const endPoint = "https://api.mercadolibre.com/sites/MLB/categories";
-const response = await fetch(endPoint);
-const object = await response.json();
-return object;
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(endPoint);
+  const object = await response.json();
+  return object;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
