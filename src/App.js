9dff62a3389936da5661f0416ac1fc930e2d26f6
Felipe Cardoso <felipedclc@gmail.com>
2021-03-09 16:48:38 -0300

    atualizando req 1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import * as api from './services/api';
 
 function App() {
+  api.getCategories().then((categories) => { console.log(categories); });
   return (
     <div className="App">
-      {api.getCategories().then((categories) => { console.log(categories); })}
+      {}
     </div>
   );
 }
