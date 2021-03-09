e8a1eee6a7723c5b0b82605c4d8ac43f54e56b9b
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-03-09 18:39:43 -0300

    requirement03

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
 import * as api from './services/api';
-import Search from './services/Search';
-import List from './services/List';
 
 function App() {
   api.getCategories().then((categories) => console.log(categories));
   return (
     <div className="App">
-      <Search />
-      <List />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route exact path="/cart" component={ Cart } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
