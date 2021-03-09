85b272a86415961b5ca4692ce7e4b9c1a499351c
OPedroAzevedo <pvas1996@gmail.com>
2021-03-09 16:41:05 -0300

    CategoryList

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import SearchBar from './SearchBar';
+import CategoryList from './CategoryList';
 
 class HomePage extends React.Component {
   render() {
     return (
       <>
+        <CategoryList />
         <SearchBar />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
