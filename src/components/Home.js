bc38aaa91bc2fd5446b6b3bee34df14907f69d01
BernardoFontes <bfontes.1997@gmail.com>
2021-03-09 18:07:25 -0300

    botao

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-        <button data-testid="shopping-cart-button" >
+        <button data-testid="shopping-cart-button" type="button">
           <Link to="/components/carrinho" component={ Carrinho } />
         </button>
       </section>
