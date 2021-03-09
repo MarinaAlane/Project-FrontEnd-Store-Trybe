62b8757e31f68f053fdd637ab73b08cb3084e0a9
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-03-09 17:04:19 -0300

    requirement 02 - go to cry

---
+++ src/Search.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react"; 
+
+class Search extends Component{
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        <h1> Digite algum termo de pesquisa ou escolha uma categoria.</h1>
+      </div>
+        
+    );
+  }
+}
+
+export default Search;
