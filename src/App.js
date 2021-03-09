8cdacefb3c31e419ffc94b0e3d94f38773cce975
Thiago Granville <thiagogranville@gmail.com>
2021-03-09 18:09:31 -0300

    [FEAT][REQUIREMENT 03][SHOPPING CART BUTTON]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ProductList from './pages/ProductList';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route exact path="/" component={ ProductList } />
+          <Route path="/cart" component={ ShoppingCart } />
         </Switch>
       </Router>
     );
--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductList extends Component {
   render() {
     return (
       <main>
-        <header>
+        <nav>
           <input type="text" />
-        </header>
+          <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
+        </nav>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
