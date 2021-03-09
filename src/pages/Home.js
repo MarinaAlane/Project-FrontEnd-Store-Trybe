6119852134b6f52c77fce27e95a32146f8cc76a2
johnealves <johne_af@hotmail.com>
2021-03-09 20:26:56 -0300

    g13-requisito-5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import ProductCard from '../Components/ProductCard';
+import * as api from '../services/api';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setSearchText = this.setSearchText.bind(this);
+    this.searchProducts = this.searchProducts.bind(this);
+
+    this.state = {
+      searchText: '',
+      productList: [],
+    };
+  }
+
+  setSearchText({ target }) {
+    this.setState({ searchText: target.value });
+  }
+
+  async searchProducts() {
+    const { searchText } = this.state;
+    const text = searchText;
+    const response = await api.getProductsFromCategoryAndQuery('', text);
+    const list = await response.results;
+    this.setState({ productList: list });
+  }
+
   render() {
+    const { productList } = this.state;
     return (
       <div className="App">
-        <input type="text" />
+        <input
+          type="text"
+          data-testid="query-input"
+          onChange={ this.setSearchText }
+        />
+        <button type="button" onClick={ this.searchProducts } data-testid="query-button">
+          Pesquisar
+        </button>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-
+        {productList
+          .map((product) => (
+            <ProductCard product={ product } key={ product.id } />
+          ))}
       </div>
     );
   }
