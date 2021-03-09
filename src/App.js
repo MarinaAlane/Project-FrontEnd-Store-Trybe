7ba4f095827237beca01459362c4f14156ccfb35
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-03-09 18:58:20 -0300

    Requisito 3 implementado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import './App.css';
 import Cart from './components/Cart';
-
 import * as api from './services/api';
 
 function App() {
