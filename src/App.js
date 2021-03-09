42d8f04c96925db5d08cfc6b2a9139a4a325787a
Christopher <chris.tryber@gmail.com>
2021-03-09 15:49:14 -0300

    Abrindo PR

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Abrindo PR</h1>
       </header>
     </div>
   );
 }
+
 
 export default App;
