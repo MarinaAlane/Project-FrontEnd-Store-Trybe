535616e62232e6432d3821cf61c950cc9e0a1d12
AmaralPH <38744941+AmaralPH@users.noreply.github.com>
2021-03-09 16:53:37 -0300

    Update SearchBar.jsx
    - Corrige erros no ESlint

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 class SearchBar extends React.Component {
   render() {
-    return(
+    return (
       <div>
         <input type="text" className="search-bar" />
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
       </div>
-    )
+    );
   }
 }
 
