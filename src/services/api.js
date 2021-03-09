07765bfc5ca55ab93bd27b384a54a4ff569667d3
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-03-09 14:22:02 -0400

    Adiciona branch do grupo 21

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ export async function getCategories() {
   // Implemente aqui
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(category, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
