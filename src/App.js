cf08e0dfcf9ff1ce048643430ba9c5d0d715828b
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-03-09 16:46:31 -0300

    requisito-2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Router, { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text"></input>
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+      {/* <Router>
+        <Switch>
+          <Route exact path="/" component="" />
+        </Switch>
+      </Router> */}
     </div>
   );
 }
