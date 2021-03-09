cc4d17b3c31827f1694d212182ddcbc96ef70d91
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-09 18:07:23 -0300

    Add try/catch in API request

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,20 @@
 export async function getCategories() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const categories = await response.json();
-  return categories;
+  try {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const categories = await response.json();
+    return categories;
+  } catch (error) {
+    throw new Error('Failed to fetch API and get categories');
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(categorieId, query) {
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categorieId}&q=${query}`;
-  const response = await fetch(endpoint);
-  const products = await response.json();
-  return products;
+  try {
+    const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categorieId}&q=${query}`;
+    const response = await fetch(endpoint);
+    const products = await response.json();
+    return products;
+  } catch (error) {
+    throw new Error('Failed to fetch API and get products');
+  }
 }
