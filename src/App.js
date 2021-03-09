ac83191df3af59b3e6aef64993cf2f58fa539c17
diego-vini <analistamm@gmail.com>
2021-03-09 17:57:00 -0300

    Requisito 4 - Listar filtros de categorias

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import * as api from './services/api';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() { this.fetchCategories(); }
+
+  async fetchCategories() {
+    this.setState({ categories: await api.getCategories() });
+  }
+
+  mapCategory() {
+    const { categories } = this.state;
+    return categories.map(
+      ({ id, name }) => <li key={ id } data-testid="category" id={ id }>{name}</li>,
+    );
+  }
+
   render() {
     return (
       <div>
-
+        <aside>
+          <ul>
+            { this.mapCategory() }
+          </ul>
+        </aside>
         <input type="text" placeholder="Digite aqui" />
 
         <p data-testid="home-initial-message">
