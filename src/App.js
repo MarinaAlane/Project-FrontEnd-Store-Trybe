91464b27687bf08dcb2e17e202c8a49db739eea5
Victor Costa <victordiricoo@gmail.com>
2021-03-09 19:19:33 -0300

    Req.03: Create ShoppingCart component and add route /shopping-cart

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import ProductList from './components/ProductList';
+import ShoppingCart from './components/ShoppingCart';
 // import fetchAPI from './services/api';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ ProductList } />
+      <Route exact path="/" component={ ProductList } />
+      <Route path="/shopping-cart" component={ ShoppingCart } />
     </BrowserRouter>
-
   );
 }
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductList extends Component {
   render() {
     return (
       <div>
         <input type="text" />
+        <Link
+          data-testid="shopping-cart-button"
+          to="/shopping-cart"
+        >
+          Carrinho de compras
+        </Link>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
