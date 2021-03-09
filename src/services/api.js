e22a4561adbe8347b1da11759924aecd366ae2c3
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-09 17:57:12 -0300

    fix fetch returns

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 export async function getCategories() {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  return response;
+  const categories = await response.json();
+  return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categorieId, query) {
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categorieId}&q=${query}`;
   const response = await fetch(endpoint);
-  return response;
+  const products = await response.json();
+  return products;
 }
