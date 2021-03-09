3d9f1284b88f330fd444ec0371f951b4badbe273
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-03-09 17:16:36 -0300

    carrinho de compras - botao e pagina

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import MainPage from './MainPage';
+import ShoppingCartBtn from './ShoppingCartBtn';
 
 class Home extends React.Component {
   render() {
     return (
       <>
         <SearchBar />
+        <ShoppingCartBtn />
         <MainPage />
       </>
     );
---
+++ src/ShoppingCart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class ShoppingCart extends React.Component {
+  render() {
+    return (
+      <section>
+        <Link to="/">
+          <button type="button">VOLTAR</button>
+        </Link>
+        <h1>Carrinho de Compras</h1>
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </section>
+    );
+  }
+}
+
+export default ShoppingCart;
