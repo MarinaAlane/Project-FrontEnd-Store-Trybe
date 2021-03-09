979e003f4f1053b38667f110a13a2e95fbece850
Lucas Silva <martssilvapro@gmail.com>
2021-03-09 16:22:43 -0300

    requisito 2 feito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as api from './services/api';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
-      <h1>blabal</h1>
-      {api.getCategories().then(categories => { console.log(categories) })}
+      <Home />
     </div>
   );
 }
