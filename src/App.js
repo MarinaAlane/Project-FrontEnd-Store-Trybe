cedad0c1b8f36f63aebbb153901b315582841d89
Luiz Vicentin <luizpavanello@gmail.com>
2021-03-09 20:24:22 -0300

    Create an empty product listing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import Cart from './pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
+      <Route exact path="/" component={ LandingPage } />
+      <Link to="/cart" data-testid="shopping-cart-button">
+        Cart
+      </Link>
       <Route path="/cart" component={ Cart } />
     </BrowserRouter>
   );
