358c124f021a2aed3e79ed3ed7af8e823cae42b4
Nícolas Godek <nicolasgodek@gmail.com>
2021-03-09 17:55:56 -0300

    lista de categorias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FilterCategories from './components/filterCategories';
 import SearchBar from './components/searchBar';
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <FilterCategories />
     </div>
   );
 }
