e44fcd2c3aeb93fe88db374e02acebc3657a189a
BernardoFontes <bfontes.1997@gmail.com>
2021-03-09 15:58:52 -0300

    adicionando requisitos 123

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Carrinho from './Carrinho';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <section>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <button data-testid="shopping-cart-button" type="button">
+          <Link to="/carrinho" component={ Carrinho } />
+        </button>
+      </section>
+    );
+  }
+}
+
+export default Home;
