0be2f546a581931a885bba0ad61c8af6ef2ec715
Pedro Henrique <pedro.876.ph@gmail.com>
2021-03-09 17:01:53 -0300

    requisito 1.1 feito

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 export async function getCategories() {
   // Implemente aqui
+  try {
+    const endpoint = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const promise = await endpoint.json();
+    return promise;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+
 }
