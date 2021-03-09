ecf25f014daa476232ba4bbd18e7225cc62a7dea
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-03-09 17:13:45 -0300

    fix/eslint

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,42 +1,42 @@
 import React from 'react';
-import * as api from '../services/api';
-
 import { Link } from 'react-router-dom';
+import * as api from '../services/api';
 
 class Home extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      categories: '',
-      products: '',
-    }
+    super(props);
     this.getCategories = this.getCategories.bind(this);
-    this.getProductsFromCategoryAndQuery = this.getProductsFromCategoryAndQuery.bind(this);
-    
+    this.getProducts = this.getProducts.bind(this);
   }
 
   componentDidMount() {
     this.getCategories();
-    this.getProductsFromCategoryAndQuery();
+    this.getProducts();
   }
 
   async getCategories() {
     await api.getCategories();
   }
 
-  async getProductsFromCategoryAndQuery(categoryId, query) {
+  async getProducts(categoryId, query) {
     await api.getProductsFromCategoryAndQuery(categoryId, query);
   }
- 
-  render() { 
+
+  render() {
     return (
       <div>
         <input type="text" />
-        <h4 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h4>
-        <button><Link to="/cart" data-testid="shopping-cart-button">Cart</Link></button>
+        <h4 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h4>
+        <button type="button">
+          <Link to="/cart" data-testid="shopping-cart-button">
+            Cart
+          </Link>
+        </button>
       </div>
     );
   }
 }
- 
+
 export default Home;
