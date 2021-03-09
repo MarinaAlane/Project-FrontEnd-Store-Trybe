67f7d476c8130fe1fab38563ab4d98a656ec32ed
marcusWittho <witthoeft.mcs@hotmail.com>
2021-03-09 18:26:32 -0300

    Adição do botão e da página do carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
+import ShopCart from './components/ShopCart';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <SearchBar />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className="App">
+          <Route path="/" component={ SearchBar } />
+          <Route path="/ShopCart" component={ ShopCart } />
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
   // Implemente aqui
+  const requestReturn = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const requestObj = await requestReturn.json();
+  return requestObj;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const requestReturn = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+  const requestObj = await requestReturn.json();
+  return requestObj;
 }
