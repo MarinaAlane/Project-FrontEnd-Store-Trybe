ac1179ca1bf9b42a7a5a3182194f3ab4388b1309
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-09 17:10:55 -0300

    Add fetch API functions

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories'); 
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+getCategories();
+
+export async function getProductsFromCategoryAndQuery(categorieId, query) {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categorieId}&q=${query}`;
+  const response = await fetch(endpoint);
+  return response;
 }
