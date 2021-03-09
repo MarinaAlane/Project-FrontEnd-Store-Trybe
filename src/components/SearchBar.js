19df8e02bf6e9f09d95cfcd694b39f08d1899c1d
Diogenes <diogenesfelipe.s@outlook.com>
2021-03-09 19:22:31 -0300

    feat/listagem-de-produtos

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
+import * as api from '../services/api';
+import Product from './Product';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.handlerChange = this.handlerChange.bind(this);
+    this.handlerClick = this.handlerClick.bind(this);
+
+    this.state = {
+      query: '',
+      products: [],
+    };
+  }
+
+  handlerChange({ target }) {
+    const { value } = target;
+    this.setState({
+      query: value,
+    });
+  }
+
+  async handlerClick() {
+    const { query } = this.state;
+    const fechProducts = await api.getProductsFromCategoryAndQuery('', query);
+    this.setState({
+      products: fechProducts.results,
+    });
+  }
+
+  showProducts() {
+    const { products } = this.state;
+    return (
+      products.map((product) => <Product key={ product.id } product={ product } />)
+    );
+  }
+
   render() {
+    const { query, products } = this.state;
     return (
       <div>
-        <input type="text" />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <section>
+          <input
+            type="text"
+            data-testid="query-input"
+            value={ query }
+            onChange={ this.handlerChange }
+          />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handlerClick }
+          >
+            Search
+          </button>
+        </section>
+        { products.length !== 0
+          ? this.showProducts()
+          : (
+            <p data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          )}
       </div>
     );
   }
