d496d7417038dc3872698de77c7867c3b0f2eb26
OPedroAzevedo <pvas1996@gmail.com>
2021-03-09 11:07:39 -0300

    Requisito 02

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <nav>
+        <input type="text" />
+        <button type="button">Buscar</button>
+      </nav>
+    );
+  }
+}
+
+export default SearchBar;
