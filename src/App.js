209fa5c7fb12db98d9faf1855d747b8282834db1
Pedro Amaral <amaralph1995@gmail.com>
2021-03-09 17:50:15 -0300

    Cria funções de fetch em api.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
 import './App.css';
+import TesteApi from './components/TesteAPI';
 
 function App() {
   return (
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 export async function getCategories() {
   // Implemente aqui
+  // estudei o código https://github.com/tryber/sd-09-project-frontend-online-store/pull/58/files
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const json = await response.json();
+  return json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`
+  const response = await fetch(endpoint);
+  const json = await response.json();
+  return json;
 }
