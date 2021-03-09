076b31c3925a04f88123c05ddd27efe3027fd725
Henrique Brito Elias <henriquebelias@gmail.com>
2021-03-09 19:00:32 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,41 +1,76 @@
 import React, { Component } from 'react';
+import ProductList from './components/ProductsList';
 import './App.css';
 import * as api from './services/api';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
     this.state = {
       categories: [],
+      productsInput: '',
+      categoriesInput: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() { this.fetchCategories(); }
+
+  handleChange({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
   async fetchCategories() {
     this.setState({ categories: await api.getCategories() });
   }
 
   mapCategory() {
-    const { categories } = this.state;
+    const { categories, categoriesInput } = this.state;
     return categories.map(
-      ({ id, name }) => <li key={ id } data-testid="category" id={ id }>{name}</li>,
+      ({ id, name }) => (
+        <label key={ id } data-testid="category" htmlFor={ id }>
+          <input
+            type="checkbox"
+            name="categoriesInput"
+            id={ id }
+            onChange={ this.handleChange }
+            value={ name }
+            checked={ categoriesInput === name }
+          />
+          { name }
+        </label>
+      ),
     );
   }
 
   render() {
+    const { productsInput, categoriesInput } = this.state;
     return (
       <div>
         <aside>
-          <ul>
+          <div>
             { this.mapCategory() }
-          </ul>
+          </div>
         </aside>
-        <input type="text" placeholder="Digite aqui" />
+        <input
+          type="text"
+          placeholder="Digite aqui"
+          value={ productsInput }
+          onChange={ this.handleChange }
+          data-testid="query-input"
+          name="productsInput"
+        />
 
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+
+        <ProductList query={ productsInput } categories={ categoriesInput } />
 
       </div>
     );
