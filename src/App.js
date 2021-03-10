fa5342c7e21c2715f8cae3168fe8fd194767b5f6
Osvaldo Borges <netoo70@gmail.com>
2021-03-10 11:20:23 -0300

    Requisito 05

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Search from './Search';
-import * as api from './services/api';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Search from './Pages/Search';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    api.getCategories()
-      .then((categories) => {
-        console.log(categories);
-      });
     return (
-      <div className="App-header">
-        <h1>Hello world!</h1>
-        <Search />
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Search } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
