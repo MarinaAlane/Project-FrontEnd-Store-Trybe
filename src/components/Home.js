b740a78534ca4cf0e571986fc056fedc5ccbbd09
Qu4k3r <neves0007@gmail.com>
2021-03-09 23:37:59 -0300

    Requisito 4 | Implementada requisicao que exibe a lista de categorias

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,20 @@
 import React from 'react';
+
 import { Link } from 'react-router-dom';
+import Categories from './Categories';
 
 class Home extends React.Component {
   render() {
     return (
       <section>
         <input />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
         <Link data-testid="shopping-cart-button" to="/components/carrinho">
           <i className="fas fa-shopping-cart" />
         </Link>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Categories />
       </section>
     );
   }
