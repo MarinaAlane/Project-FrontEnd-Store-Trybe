7d662961592207eb1560ddc737aa25bbfedf6d2c
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-03-10 14:38:53 -0400

    Refatorado

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 export async function getCategories() {
   const categoriesApi = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const categoriesApiJson = categoriesApi.json();
+  const categoriesApiJson = await categoriesApi.json();
   return categoriesApiJson;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const urlApi = 'https://api.mercadolibre.com/sites/MLB/search?category=';
   const categorieAndQueryApi = await fetch(`${urlApi}${categoryId}_ID&q=${query}`);
-  const categorieAndQueryApiJson = categorieAndQueryApi.json();
+  const categorieAndQueryApiJson = await categorieAndQueryApi.json();
   return categorieAndQueryApiJson;
 }
