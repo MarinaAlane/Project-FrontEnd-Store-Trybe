3fc4d607c267d4ddee687cf307417981691c82d7
Pedro Castro <pa.coelho.castro@gmail.com>
2021-03-10 18:52:38 -0300

    Group 21 - Adiciona requisito 10

--- src/components/ItemCart.js
+++ src/components/ItemCart.js
@@ -2,7 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class ItemCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 1,
+    };
+    this.decreaseQtt = this.decreaseQtt.bind(this);
+    this.increaseQtt = this.increaseQtt.bind(this);
+  }
+
+  decreaseQtt() {
+    this.setState((previousState) => ({
+      quantity: previousState.quantity - 1,
+    }));
+  }
+
+  increaseQtt() {
+    this.setState((previousState) => ({
+      quantity: previousState.quantity + 1,
+    }));
+  }
+
   render() {
+    const { quantity } = this.state;
     const { item } = this.props;
     const { title, price, thumbnail } = item;
     return (
@@ -10,9 +32,24 @@ class ItemCart extends Component {
         <h4 data-testid="shopping-cart-product-name">{ title }</h4>
         <p>{ price }</p>
         <img src={ thumbnail } alt={ title } />
+        <br />
+        <button
+          type="button"
+          onClick={ this.decreaseQtt }
+          data-testid="product-decrease-quantity"
+        >
+          -
+        </button>
         <p data-testid="shopping-cart-product-quantity">
-          1
+          { quantity }
         </p>
+        <button
+          type="button"
+          onClick={ this.increaseQtt }
+          data-testid="product-increase-quantity"
+        >
+          +
+        </button>
       </div>
     );
   }
