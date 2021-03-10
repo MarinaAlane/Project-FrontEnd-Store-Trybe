5b22c58b9f5f6db2aceb8850dfb88caa524ae1a0
Marconi <marconieng@gmail.com>
2021-03-10 16:55:22 -0300

    Adiciona requisito 3

--- src/Pages/ProductsShowcase.js
+++ src/Pages/ProductsShowcase.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ButtonShoppingCart from '../components/ButtonShoppingCart';
 
 class ProductsShowcase extends React.Component {
   render() {
     return (
       <div>
         <input />
+        <Link data-testid="shopping-cart-button" to="/shopping-cart">
+          <ButtonShoppingCart />
+        </Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
