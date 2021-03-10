4b80aeb9fc1c0db04477e09127ee97899fbfd026
cleber-teixeira <cleberlt@gmail.com>
2021-03-10 17:39:45 -0300

    Ex.4 - Liste as categorias de produtos disponíveis via API na página principal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Cart from './pages/Cart';
+import ListCategories from './pages/ListCategories';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
-      <Route path="/cart" component={ Cart } />
-    </BrowserRouter>
+    <div>
+      <ListCategories />
+      <BrowserRouter>
+        <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
+        <Route path="/cart" component={ Cart } />
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/pages/ListCategories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as api from '../services/api';
+
+class ListCategories extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories()
+      .then((response) => this
+        .setState({ categories: response }));
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        {categories.map((category) => (
+          <label htmlFor="category" key={ category.id }>
+            <input
+              type="radio"
+              value={ category.name }
+              key={ category.id }
+              name="category"
+              data-testid="category"
+            />
+            { category.name }
+          </label>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ListCategories;
