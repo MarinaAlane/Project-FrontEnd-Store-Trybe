53f0aa9d830ecd1dd98b21a1861116d07d573bed
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-03-10 17:38:29 -0300

    Group 21 - Adiciona requisito 8

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import ItemCart from './ItemCart';
 
 class ShoppingCart extends Component {
   render() {
-    return (
-      <div>
+    const { cartItems } = this.props;
+    if (cartItems.length < 1) {
+      return (
         <h3
           data-testid="shopping-cart-empty-message"
         >
           Seu carrinho está vazio
         </h3>
+      );
+    }
+    return (
+      <div>
+        {cartItems.map((item) => <ItemCart key={ item.id } item={ item } />)}
       </div>
     );
   }
 }
+
+ShoppingCart.propTypes = {
+  cartItems: PropTypes.arrayOf(),
+}.isRequired;
 
 export default ShoppingCart;
