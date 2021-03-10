89a788ab6e710ff1f7bd7930d5dacf0aade37261
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-03-10 09:37:16 -0300

    Group 21 - Adiciona requisito 6

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import { getCategories } from '../services/api';
 
 class Categories extends Component {
@@ -23,6 +25,7 @@ class Categories extends Component {
 
   render() {
     const { categories } = this.state;
+    const { onClick } = this.props;
 
     if (categories.length < 1) {
       return <h4>Loading</h4>;
@@ -31,20 +34,28 @@ class Categories extends Component {
     return (
       <div>
         <h1>Categorias</h1>
-        <ul>
-          {categories.map((catObj) => (
-            <li
-              key={ catObj.id }
-              data-testid="category"
-              name={ catObj.id }
-            >
+        {categories.map((catObj) => (
+          <div key={ catObj.id }>
+            <label htmlFor="category-name">
+              <input
+                type="radio"
+                id={ catObj.id }
+                name="category-name"
+                data-testid="category"
+                value={ catObj.id }
+                onClick={ onClick }
+              />
               { catObj.name }
-            </li>
-          ))}
-        </ul>
+            </label>
+          </div>
+        ))}
       </div>
     );
   }
 }
+
+Categories.propTypes = {
+  onClick: PropTypes.func,
+}.isRequired;
 
 export default Categories;
