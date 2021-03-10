369e124adaf3b5cc7d6ebd002487eaeed5d059d9
Jorge William Cardoso Furtado <jorge_william_cardoso@yahoo.com.br>
2021-03-10 18:49:22 -0300

    Requisito: :seven:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import ShoppingCart from './ShoppingCart';
+import ProductDetails from './ProductDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/shoppingcart" component={ ShoppingCart } />
+          <Route exact path="/productDetails" component={ ProductDetails } />
         </Switch>
       </BrowserRouter>
     </div>
