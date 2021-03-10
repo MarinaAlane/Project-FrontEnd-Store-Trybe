4e6a70d998c5038490394e34dd15d5df3929ac49
LucioOSilva <lucioosilva00@gmail.com>
2021-03-09 22:37:03 -0300

    [grupo19] - correçao de lint em Appjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ProductList } from './pages';
-import { ShoppingCart } from './pages';
+import { ProductList, ShoppingCart } from './pages';
 
 function App() {
   return (
