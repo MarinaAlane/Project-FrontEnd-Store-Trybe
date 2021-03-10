8fb8d2aaf72fe690de8e139688c2fed3cd45d42b
Luciano_Lodi <lrlodi@gmail.com>
2021-03-10 19:23:15 -0300

    Requisito 6

--- src/services/api.js
+++ src/services/api.js
@@ -16,6 +16,12 @@ export async function getProductsFromQuery(query) {
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
   const productsPromisse = await fetch(endpoint);
   const productsJson = await productsPromisse.json();
-  console.log(productsJson);
+  return productsJson;
+}
+
+export async function getProductsFromCategory(category) {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${category}`;
+  const productsPromisse = await fetch(endpoint);
+  const productsJson = await productsPromisse.json();
   return productsJson;
 }
