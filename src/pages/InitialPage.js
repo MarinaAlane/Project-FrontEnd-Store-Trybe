100eb6b6236059a293014f1e1b70557f14c6b8f7
Nícolas Tozato <ntozato9@gmail.com>
2021-03-10 10:16:53 -0300

    requisito 2

---
+++ src/pages/InitialPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class InitialPage extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default InitialPage;
