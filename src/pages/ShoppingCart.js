90673b41faf5ca230a60c00d5ab35d6b12522983
Nícolas Tozato <ntozato9@gmail.com>
2021-03-10 17:33:29 -0300

    centraliza mensagem do carrinho

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,7 +3,9 @@ import React from 'react';
 class ShoppingCart extends React.Component {
   render() {
     return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      <div className="App">
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </div>
     );
   }
 }
