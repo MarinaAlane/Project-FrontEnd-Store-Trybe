4b5a19e8c44dba00e3e575b1df878c756aa4ff8d
Khyrki <khyrkimitsunobi@gmail.com>
2021-03-10 19:04:15 -0300

    ADD requisito 4

--- src/Pages/ProductsShowcase.js
+++ src/Pages/ProductsShowcase.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ButtonShoppingCart from '../components/ButtonShoppingCart';
+import CategoriesList from '../components/CategoriesList';
+import * as api from '../services/api';
 
 class ProductsShowcase extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesArray: '',
+    };
+  }
+
+  componentDidMount() {
+    return api.getCategories().then((categories) => (
+      this.setState({ categoriesArray: categories })
+    ));
+  }
+
   render() {
+    const { categoriesArray } = this.state;
     return (
       <div>
         <input />
@@ -13,6 +29,9 @@ class ProductsShowcase extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <CategoriesList
+          categoriesArray={ categoriesArray }
+        />
       </div>
     );
   }
