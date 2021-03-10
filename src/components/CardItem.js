560e2ad097a72c9cbbb644ce7277e680d8ea4d26
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2021-03-10 12:33:42 -0400

    Passa Id para Url pelo componente CardItem

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class CardItem extends Component {
   render() {
-    const { item: { title, price, thumbnail } } = this.props;
+    const { item: { title, price, thumbnail, id } } = this.props;
     return (
-      <div data-testid="product">
-        <h4>{ title }</h4>
-        <p>{ price }</p>
-        <img src={ thumbnail } alt={ title } />
-      </div>
+      <Link to={ `/details/${id}` }>
+        <div data-testid="product">
+          <h4>{ title }</h4>
+          <p>{ price }</p>
+          <img src={ thumbnail } alt={ title } />
+        </div>
+      </Link>
     );
   }
 }
@@ -19,6 +22,7 @@ CardItem.propTypes = {
     title: PropTypes.string,
     price: PropTypes.number,
     thumbnail: PropTypes.string,
+    id: PropTypes.string,
   }).isRequired,
 };
 
