9eb266cf0854b7024fcb0a884282a3e9863bd901
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-03-10 17:55:18 -0300

    searching for error task4

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from './services/api';
+import Categories from './Categories';
 
 import './Home.css';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    getCategories().then((res) => {
+      console.log(res);
+      this.setState = {
+        categories: res,
+      };
+    });
+  }
+
   render() {
     const message = (
       <h5
@@ -13,6 +31,7 @@ class Home extends Component {
         Digite algum termo de pesquisa ou escolha uma categoria.
       </h5>
     );
+    const { categories } = this.state;
     return (
       <div>
         <input type="text" className="searchInput" />
@@ -24,6 +43,21 @@ class Home extends Component {
         >
           Botão
         </Link>
+        {
+          categories.map((category) => (
+            <label
+              htmlFor={ category.id }
+              data-testid="category"
+              key={ category.id }
+            >
+              { category.name }
+              <input
+                type="radio"
+                id={ category.id }
+                key={ category.id }
+              />
+            </label>))
+        }
       </div>
     );
   }
