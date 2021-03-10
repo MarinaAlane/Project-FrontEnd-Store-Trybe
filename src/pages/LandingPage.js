c0bb5957c89981aa38712b62423999e8c9b4a14b
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-10 16:30:12 -0300

    Card component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getProductsFromCategoryAndQuery } from '../services/api';
+import Card from '../components/Card';
 
 class LandingPage extends React.Component {
 
@@ -15,20 +16,28 @@ class LandingPage extends React.Component {
 
   async fetchProductsByQuery(query) {
     const items = await getProductsFromCategoryAndQuery(false, query);
-    // console.log(items);
     this.setState({
       products: items.results,
     });
   }
 
   render() {
-    this.fetchProductsByQuery('computador');
+    const { products } = this.state;
+
     return (
       <div>
-        <input type="text" />
+        <input type="text" data-testid="query-input" />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ () => this.fetchProductsByQuery('computador') }
+        >
+          Pesquisar
+        </button>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        {products.map((product) => <Card product={ product } key={ product.id } />)}
       </div>
     );
   }
