68e4cbea5a23f206606fdb90de2a3f61792fd244
Khyrki <khyrkimitsunobi@gmail.com>
2021-03-10 15:05:46 -0300

    realiza o pull das informacoes da branch main-group-25

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import './App.css';
-import * as api from './services/api';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.css';
+import * as api from './services/api';
 import ProductsShowcase from './components/ProductsShowcase';
 
 function App() {
   api.getCategories().then((categories) => { console.log(categories); });
   return (
-    <div>
-      {}
-    </div>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ ProductsShowcase } />
