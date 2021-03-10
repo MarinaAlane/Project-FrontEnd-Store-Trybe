98f7b56a0c5bedb7e820abc9ae95b00077067857
marcusWittho <witthoeft.mcs@hotmail.com>
2021-03-09 23:58:46 -0300

    Adicionando a imagem do carrinho

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import shopCart from '../images/shopCart.png';
 
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <input type="text" className="search-bar" />
+      <>
+        <div className="header">
+          <input type="text" className="search-bar" />
+
+          <Link to="/shopping-cart" data-testid="shopping-cart-button">
+            <img src={ shopCart } alt="Imagem do carrinho de compras" className="shop-cart" />
+          </Link>
+        </div>
+
         <h4 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h4>
-        <button type="button">
-          <Link to="/shopping-cart" data-testid="shopping-cart-button">
-            Carrinho de compras
-          </Link>
-        </button>
-      </div>
+      </>
     );
   }
 }
