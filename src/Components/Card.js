a094397252c2c1a66ed912613f9cebb4c1127dfd
Arlesson Moura <arlessonmss@gmail.com>
2021-03-11 18:34:49 -0300

    Requirement 07

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
+import { Link } from 'react-router-dom';
 
 class Card extends React.Component {
   render() {
-    const { product: { title, thumbnail, price } } = this.props;
+    const { product } = this.props;
+    const { title, thumbnail, price } = product;
 
     return (
       <div data-testid="product" className="card">
         <p className="card-title">{title}</p>
         <img className="card-img" src={ thumbnail } alt={ title } />
         <p className="card-price">{`R$ ${price}`}</p>
+        <Link to={ { pathname: "/product", state: { product } } } data-testid="product-detail-link">Detalhes do produto</Link>
       </div>
     );
   }
