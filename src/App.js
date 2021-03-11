ad3ddf16aaf19112f7598cf6fb35be3657528d84
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-03-11 16:18:45 -0300

    Requisito 1 ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 import './App.css';
 
 function App() {
+  { getCategories() }
+  { getProductsFromCategoryAndQuery("MLB1648", "I7") }
   return (
     <div className="App">
       
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 export async function getCategories() {
   // Implemente aqui
+  const endPoint =  'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(endPoint);
+  const categories = await response.json();
+  // console.log(categories);
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const response = await fetch(endPoint);
+  const products = await response.json();
+  // console.log(products);
+  return products;
 }
