2d8b275a4938c798508e113dcd592b3b4c47018f
Arlesson Moura <arlessonmss@gmail.com>
2021-03-11 18:53:03 -0300

    Deep extrict ESLint requirements

--- src/Pages/SingleView.jsx
+++ src/Pages/SingleView.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import imgKart from '../Images/botaoCarrinho.jpg';
-import './SingleView.css';
 
 class SingleView extends React.Component {
   render() {
