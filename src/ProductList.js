a2f2c4f4cc049ac653f60d2de7c75b6112baa8c2
Carine Becker <caarine.m@gmail.com>
2021-03-11 15:37:52 -0300

    render categories in ProductList

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import ButtonCart from './components/shopping_cart/ButtonCart';
+import AsideCategoriesList from './components/AsideCategoriesList';
 
 class ProductList extends Component {
   render() {
     return (
       <div className="product-list">
         <input type="text" className="search-bar" />
-        <ButtonCart />
         <p data-testid="home-initial-message" className="product-list-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <AsideCategoriesList />
+        <ButtonCart />
       </div>
     );
   }
