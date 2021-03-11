e54702a4cc38495d82463a06aa63e63a427813b4
Enio Nicael <enionicael@gmail.com>
2021-03-10 22:18:33 -0300

    Requisito 2, cria um componete Header e renderiza na Home.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
+import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <Route to="/" component={} />
+        <Route to="/" component={Home} />
       </Router>
     );
   }
