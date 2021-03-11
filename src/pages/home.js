f5f1a08497191b3df27835c71fc5383c80e921a7
Perycles floriano <perycles_reis@hotamil.com>
2021-03-11 11:02:18 -0300

    requisito 4

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
-// requisito 2
+import * as apis from '../services/api';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+    this.apiRequest = this.apiRequest.bind(this);
+  }
+
+  componentDidMount() {
+    this.apiRequest();
+  }
+
+  async apiRequest() {
+    const { getCategories } = apis;
+    const result = await getCategories();
+    this.setState({
+      categories: [...result],
+    });
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +29,7 @@ class Home extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        {this.state.categories.map((categoria) => (<div> {categoria.name} </div>))}
       </div>
     );
   }
