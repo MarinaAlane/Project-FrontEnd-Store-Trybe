d2ad30f6d9270fd9dafb48afd301688cb572964f
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-03-11 17:13:47 -0300

    Implementação do requisito 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
-    <div className="App" />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ MainPage } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
