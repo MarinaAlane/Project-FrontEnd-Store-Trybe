5ed86d78224ade729df8b0f9b049a8fdb5000b27
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-11 18:16:28 -0300

    Add form Handlers

--- src/components/review_form/ReviewForm.jsx
+++ src/components/review_form/ReviewForm.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
 
 class ReviewForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'Anônimo',
+      stars: 0,
+      textReview: '',
+      reviews: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange({ target }) {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { email, stars, textReview } = this.state;
+    const newReview = {
+      email,
+      stars,
+      textReview,
+    };
+    this.setState(({ reviews }) => {
+      const updatedReviews = [...reviews, newReview];
+      return { reviews: updatedReviews };
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Avaliação</h2>
         <form>
-          <input type="text" name="name" placeholder="Email" />
-          <select>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={ this.handleChange }
+          />
+          <select name="stars" onChange={ this.handleChange } required>
             <option value="1">1 estrela</option>
             <option value="2">2 estrela</option>
             <option value="3">3 estrela</option>
@@ -15,9 +53,14 @@ class ReviewForm extends Component {
             <option value="5">5 estrela</option>
           </select>
           <br />
-          <textarea placeholder="Mensagem (opcional)" />
+          <textarea
+            data-testid="product-detail-evaluation"
+            name="textReview"
+            placeholder="Mensagem (opcional)"
+            onChange={ this.handleChange }
+          />
           <br />
-          <input type="submit" value="Avaliar" />
+          <input type="submit" value="Avaliar" onClick={ this.handleSubmit } />
         </form>
       </div>
     );
