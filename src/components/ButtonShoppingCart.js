9f44ead9392cd724ebb5aed5b7cbae765c0062e5
Enio Nicael <enionicael@gmail.com>
2021-03-11 16:13:58 -0300

    Requisito 3 cria um botao com icone de carrinho de compras.

---
+++ src/components/ButtonShoppingCart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa'
+
+class ButtonShoppingCart extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link to="/shopping-cart" data-testid="shopping-cart-button" >
+          <FaShoppingCart />
+        </Link>
+      </div>
+    );
+  }
+}
+// 
+export default ButtonShoppingCart;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import ButtonShoppingCart from './ButtonShoppingCart';
+
 class Header extends React.Component {
   render() {
     return (
       <header>
         <input type="search" name="" id="" />
+        <ButtonShoppingCart />
       </header>
     );
   }
