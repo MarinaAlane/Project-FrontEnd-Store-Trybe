813544f41464bcb5fa1c02d66d076a017ab4098f
Enio Nicael <enionicael@gmail.com>
2021-03-10 21:37:51 -0300

    Requisito 1: alteracoes do code review.

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
-  const fetchUrl = await fetch(url);
-  const response = await fetchUrl.json();
+  const fetchResponse = await fetch(url);
+  const response = await fetchResponse.json();
   return response;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  const fetchUrl = await fetch(url);
-  const response = await fetchUrl.json();
+  const fetchResponse = await fetch(url);
+  const response = await fetchResponse.json();
   return response;
 }
