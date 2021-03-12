438dbe01615eaca6b5ca8a2ed993fe7d8dd119db
Micaela Alves <micaela.alves93@gmail.com>
2021-03-12 18:30:19 -0300

    requisito 7 - detalhes de produto

---
+++ src/components/ProductDetailed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ProductDetailed extends React.Component {
+  render() {
+    return (
+      <section>ProductDelailed</section>
+    );
+  }
+}
+
+export default ProductDetailed;
