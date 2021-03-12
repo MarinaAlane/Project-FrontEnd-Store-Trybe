e9652ccbd8507e195bcdda375068ece9fbcee81b
Micaela Alves <micaela.alves93@gmail.com>
2021-03-12 19:19:36 -0300

    requisito 7 - page Detalhe produto e navegaçao

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ShoppingCart from './components/shopping_cart/ShoppingCart';
 import ProductList from './ProductList';
+import ProductDetailed from './components/ProductDetailed';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ ProductList } />
         <Route path="/ShoppingCart" component={ ShoppingCart } />
+        <Route path="/product" component={ ProductDetailed } />
       </Switch>
     </BrowserRouter>
   );
--- src/components/ProductDetailed.js
+++ src/components/ProductDetailed.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ButtonCart from './shopping_cart/ButtonCart';
+import ArrowBack from './shopping_cart/logo_arrow_back.svg';
+
+
 
 class ProductDetailed extends React.Component {
   render() {
     return (
-      <section>ProductDelailed</section>
+      <section>
+        <Link to="/">
+          <img src={ ArrowBack } alt="logo arrow back" />
+        </Link>
+        <Link to="/ShoppingCart">
+          <ButtonCart />
+        </Link>
+      </section>
     );
   }
 }
