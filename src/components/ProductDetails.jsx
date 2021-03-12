461f6786073dc8fe78ccc3cdb976a488f4607d0d
Marina Alane <marina.alane@outlook.com>
2021-03-12 19:23:17 -0300

    teste

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -8,11 +8,8 @@ class ProductDetails extends React.Component {
   constructor() {
     super();
     this.fetchCategory = this.fetchCategory.bind(this);
-    this.addToCart = this.addToCart.bind(this);
-
     this.state = {
       product: [],
-      shoppingCart: [],
     };
   }
 
@@ -25,29 +22,14 @@ class ProductDetails extends React.Component {
     const productObj = await api.getProductsFromCategoryAndQuery(categoryID, '');
     const productDetails = productObj.results
       .find((product) => product.id === id);
-
     this.setState({
       product: productDetails,
     });
   }
 
-  addToCart() {
-    const { product } = this.state;
-    product.quantity = 1;
-
-    this.setState({
-      shoppingCart: [product],
-    });
-  }
-
   render() {
-
     const { product: { title, thumbnail, price } } = this.state;
     console.log(this.props);
-
-    const { product, shoppingCart } = this.state;
-    const { title, thumbnail, price } = product;
-
     return (
       <div>
         <p>
@@ -64,36 +46,13 @@ class ProductDetails extends React.Component {
           </span>
         </p>
         <img src={ thumbnail } alt="product-thumbnail" />
-
         <RatingForm />
         <Link to="/shopping-cart">Ir para carrinho</Link>
-
-        <Link
-          data-testid="shopping-cart-button"
-          to={ {
-            pathname: '/shopping-cart',
-            state: shoppingCart,
-          } }
-        >
-          Ir para carrinho
-        </Link>
-        <button
-          type="button"
-          data-testid="product-detail-add-to-cart"
-          onClick={ this.addToCart }
-        >
-          Adicionar ao Carrinho
-        </button>
-
       </div>
     );
   }
 }
-// teste1
 ProductDetails.propTypes = {
-  location: PropTypes.shape({
-    state: PropTypes.arrayOf(),
-  }).isRequired,
   match: PropTypes.shape({
     params: PropTypes.shape({
       categoryID: PropTypes.string,
@@ -101,5 +60,4 @@ ProductDetails.propTypes = {
     }).isRequired,
   }).isRequired,
 };
-
 export default ProductDetails;
