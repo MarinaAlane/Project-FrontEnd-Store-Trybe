1d3d3a5b2d3b2e655f54a9abbe1c3fe0138c4d6f
Daniela Muller <danielamuller20@gmail.com>
2021-03-12 10:23:28 -0300

    fix conflicts

--- src/pages/ProductsPage/ProductsPage.jsx
+++ src/pages/ProductsPage/ProductsPage.jsx
@@ -11,6 +11,7 @@ class ProductsPage extends Component {
       product: [],
       loading: true,
       inputValue: '',
+      category: '',
       productID: [],
     };
     this.fetchAds = this.fetchAds.bind(this);
@@ -18,9 +19,12 @@ class ProductsPage extends Component {
   }
 
   componentDidUpdate() {
-    const { inputValue: stateInput } = this.state;
-    const { inputValue: contextInput } = this.context;
-    if (stateInput !== contextInput) this.fetchAds(contextInput);
+    const { inputValue: stateInput, category: stateCategory } = this.state;
+    const { inputValue: contextInput, selectedCategory: contextCategory } = this.context;
+    if (stateInput !== contextInput || stateCategory !== contextCategory) {
+      console.log('teste');
+      this.fetchAds(contextCategory, contextInput);
+    }
   }
 
   handleClick(index) {
@@ -29,13 +33,11 @@ class ProductsPage extends Component {
       element[index].innerText] }));
   }
 
-  fetchAds(inputValue) {
-    api.getProductsFromCategoryAndQuery(null, inputValue)
-      .then((term) => this.setState({ product: term, loading: false, inputValue }));
-  }
-
-  click() {
-    console.log('clicou');
+  fetchAds(category, inputValue) {
+    api.getProductsFromCategoryAndQuery(category, inputValue)
+      .then((term) => (
+        this.setState({ product: term, loading: false, inputValue, category })
+      ));
   }
 
   render() {
