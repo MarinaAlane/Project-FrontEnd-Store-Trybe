269b8ed5b2b583141fb89e85b97f6c18477f6565
Hudson Carlos <hc88806@gmail.com>
2021-03-12 10:11:52 -0400

    correcting errors

--- src/Components/ListCategorie.js
+++ src/Components/ListCategorie.js
@@ -2,28 +2,39 @@ import React, { Component } from 'react';
 import * as api from '../services/api';
 
 class ListCategorie extends Component {
-  constructor(){
+  constructor() {
     super();
     this.SearchCategorie = this.SearchCategorie.bind(this);
     this.state = {
       categories: [],
-    }
+    };
   }
 
-  componentDidMount(){
-    this.SearchCategorie()
+  componentDidMount() {
+    this.SearchCategorie();
   }
 
   SearchCategorie() {
-  api.getCategories().then((categories) => this.setState( {categories: categories} ));
+    api.getCategories().then((cat) => this.setState({
+      categories: cat,
+    }));
   }
+
   render() {
     const { categories } = this.state;
     if (categories.length === 0) return <h1>carregando</h1>;
+
     return (
-        <ol>
-          {categories.map(categorie =>  <li data-testid='category'>{categorie.name}</li>)}
-        </ol>
+      <ol>
+        {categories.map((categorie, index) => (
+          <li
+            data-testid="category"
+            key={ index }
+          >
+            { categorie.name }
+          </li>
+        ))}
+      </ol>
     );
   }
 }
