eeecf88e45edf98cd5a63468dee33e465bc90cfb
jorgeramires <jorgerl123@gmail.com>
2021-03-12 18:33:56 -0300

    FEAT/Reqisito 4 e 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,86 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-// import * as api from '../services/api';
-
+import * as api from '../services/api';
 import './home.css';
+import Product from '../components/Product';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      value: '',
+      categoryId: '',
+      products: [],
+    };
+    this.fetchCategories = this.fetchCategories.bind(this);
+    this.HandleClick = this.HandleClick.bind(this);
+    this.fetchQuery = this.fetchQuery.bind(this);
+  }
+  componentDidMount() {
+    this.fetchCategories();
+    
+  }
+
+  fetchQuery(categoryId, value) {
+    api.getProductsFromCategoryAndQuery(categoryId, value).then(({ results }) => this.setState({ products: results }))
+  }
+
+  fetchCategories() {
+    api.getCategories().then(category => this.setState({ categories: category }))
+  }
+
+  HandleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  HandleClick() {
+    const value = this.state.value;
+    this.fetchQuery(null, value)
+
+  }
+
   render() {
+    const { products, categories } = this.state;
+    //console.log(products)
     return (
       <>
         <header className="home-header">
-          <input type="text" placeholder="Digite algum termo de pesquisa" />
+          <input type="text"
+            placeholder="Digite algum termo de pesquisa"
+            data-testid="query-input"
+            onChange={this.HandleChange}
+          />
           <Link to="/cart" data-testid="shopping-cart-button">
             <i className="fas fa-shopping-cart" />
           </Link>
         </header>
-
+        <button type="button"
+          data-testid="query-button"
+          onClick={this.HandleClick} >pesquisar</button>
         <main>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         </main>
+        <div>
+          <ul>
 
-        {
-          // // Exibindo as categorias
-          // api.getCategories()
-          //   .then((categories) => console.log(categories))
-        }
+            {(categories.length > 0) && (
+              categories.map(({ id, name }) => <li key={id} data-testid="category">{name}</li>)
+            )}
+          </ul>
+        </div>
+        <div>
+          {/* {products.map(product => product.title)} */}
+          {(products.length === 0) ? (<p>Nenhum produto encontrado</p>) : (
+            products.map((product) => <Product key={product.id} array={product} />)
+          )}
+        </div>
         {
           // // Estrutura para termo
-          // api.getProductsFromCategoryAndQuery(null, 'cinta')
-          //   .then((search) => console.log(search))
+          //api.getProductsFromCategoryAndQuery(query, 'cinta')
+          //.then((search) => console.log(search))
         }
         {
           // // Estrutura para id de categoria
