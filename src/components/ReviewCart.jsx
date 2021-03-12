2fc7a4847e0c826645e88e11fa18381004321da0
Nícolas Godek <nicolasgodek@gmail.com>
2021-03-12 17:53:55 -0300

    Requisito 12 com algumas falhas

---
+++ src/components/ReviewCart.jsx
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react';
+// import renderCartItem from './ShoppingCart';
+
+// class ReviewCart extends Component {
+//   render() {
+//     return (
+//     );
+//   }
+// }
+
+// export default ReviewCart;
