0b2168bcb775519dad651dc709e5d9478bfdb5c5
Leticia Lima <letylima96@gmail.com>
2021-03-12 21:03:30 -0300

    FIX-revert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,7 +63,7 @@ class Home extends React.Component {
         >
           pesquisar
         </button>
-        <main>
+        <main className="container">
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
