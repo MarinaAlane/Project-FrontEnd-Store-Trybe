9ef451e99ac0242d105799ae2356fb5ced1b50a7
Daniela Muller <danielamuller20@gmail.com>
2021-03-13 11:20:37 -0300

    cheanges quantity items in the cart

--- src/components/CartProduct/CartProduct.jsx
+++ src/components/CartProduct/CartProduct.jsx
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { number, string, shape } from 'prop-types';
 import InputContext from '../InputContext';
+import IncrementDecrementButton from '../IncrementDecrementButton';
 
 class CartProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantityItems: 1,
+    };
+    this.incrementOnClick = this.incrementOnClick.bind(this);
+    this.incrementOnClick = this.incrementOnClick.bind(this);
+  }
+
+  incrementOnClick(value) {
+    this.setState((prevState) => ({ quantityItems: prevState.quantityItems + value }));
+  }
+
+  decrementOnClick() {
+    const { quantityItems } = this.state;
+    if (quantityItems > 0) {
+      this.setState((prevState) => ({ quantityItems: prevState.quantityItems - 1 }));
+    }
+  }
+
   render() {
     const { info } = this.props;
     const { title, thumbnail, price, quantity } = info;
+    const { quantityItems } = this.state;
+    const result = price * quantityItems;
     return (
       <InputContext.Consumer>
         {
@@ -13,8 +36,12 @@ class CartProduct extends Component {
             <section>
               <p data-testid="shopping-cart-product-name">{title}</p>
               <img src={ thumbnail } alt={ title } />
-              <p>{price}</p>
-              <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
+              <p>{Math.round(result * 100) / 100}</p>
+              <p data-testid="shopping-cart-product-quantity">{ quantityItems }</p>
+              <IncrementDecrementButton
+                increaseOnClick={ () => this.incrementOnClick(quantity) }
+                decreaseOnClick={ () => this.decrementOnClick() }
+              />
             </section>
           )
         }
