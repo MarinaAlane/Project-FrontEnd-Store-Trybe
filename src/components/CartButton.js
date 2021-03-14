8550dd06681ea4c7b40f3afc612f79f79d150cd6
Thiago Granville <thiagogranville@gmail.com>
2021-03-13 22:01:17 -0300

    [FEAT][REQUIREMENT 06][CATEGORY SEARCH]

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -5,7 +5,7 @@ class CartButton extends Component {
   render() {
     return (
       <Link data-testid="shopping-cart-button" to="/cart">
-        Carrinho
+        Cart
       </Link>
     );
   }
