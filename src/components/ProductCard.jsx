c50ce49ecb97ff9ce99d6e6b13c27816561efbf5
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-03-15 16:29:18 -0300

    Tentando Resolver problema com test 06

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ class ProductCard extends React.Component {
     const { title, thumbnail, price } = data;
     if (Object.keys(data).length) {
       return (
-        <div data-testid="product">
+        <div data-testid="product" className="products">
           <h2>{ title }</h2>
           <img src={ thumbnail } alt={ title } />
           <p>{`R$ ${price}` }</p>
