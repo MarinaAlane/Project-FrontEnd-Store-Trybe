dbf29d2340af8c5a77941118a5ff2ecdb717660e
Miguel Dantas <miiguelalb@gmail.com>
2021-03-15 13:53:40 -0300

    Código completamente refatorado e REQ6 concluído #VQV

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,14 @@
 export async function getCategories() {
-  const results = fetch('https://api.mercadolibre.com/sites/MLB/categories').then((data) => data.json())
-    .then((result) => result);
-  return results;
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(endpoint);
+  const data = response.json();
+  return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const req = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-  const results = await req.json();
-  return results || {};
+  const endpointCatandQuery = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`;
+  console.log(endpointCatandQuery);
+  const response = await fetch(endpointCatandQuery);
+  const data = response.json();
+  return data;
 }
