d2d950ebfd9b661fa333e1d8b66b0945be481766
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-03-15 16:21:45 -0300

    requisito6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import InitialPage from './pages/InitialPage';
-import ProductList from './pages/ProductList';
 import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Switch>
         <Route exact path="/" component={ InitialPage } />
         <Route path="/ShoppingCart" component={ ShoppingCart } />
-        <Route exact path="/" component={ ProductList } />
       </Switch>
     </BrowserRouter>
   );
