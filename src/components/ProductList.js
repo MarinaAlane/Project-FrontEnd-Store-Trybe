800d27fb78f9b850f5e1c9aaf5ebb1e8a8027821
Henrique-Moura <73261812+Henrique-Moura@users.noreply.github.com>
2021-03-15 14:27:53 -0300

    Update src/components/ProductList.js
    Co-authored-by: Brenno Calado <66017138+brenno-calado@users.noreply.github.com>

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -30,6 +30,11 @@ class ProductList extends React.Component {
     this.LoadCategories();
     this.loadProduct();
   }
+  
+  componentDidUpdate(prevProp, prevState) {
+    const { selectedCategories } = this.state;
+    if (prevState.selectedCategories !== selectedCategories) return this.handleSearch();
+  }
 
   async handleSearch() {
     const { searchText, selectedCategories } = this.state;
