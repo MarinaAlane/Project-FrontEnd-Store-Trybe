a9111e07694cd403b1d7beda48dd691ff9ab882a
brenno-calado <brenno.calado@gmail.com>
2021-03-15 15:44:14 -0300

    [Grupo 28] - Requisito 07

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductItem extends Component {
   render() {
@@ -9,7 +10,21 @@ class ProductItem extends Component {
       <div className="product-item" data-testid="product">
         <h4>{product.title}</h4>
         <img alt={ `${product.title}` } src={ product.thumbnail } />
-        <i>{ product.price }</i>
+        <p>{ product.price }</p>
+        <br />
+        {/* utilizei a explicação aqui ->https://ui.dev/react-router-v4-pass-props-to-link/<-
+          para passar product pelo Link */}
+        <button type="button" className="details-button">
+          <Link
+            to={ {
+              pathname: `/product/${product.id}`,
+              state: { details: product },
+            } }
+            data-testid="product-detail-link"
+          >
+            Detalhes
+          </Link>
+        </button>
       </div>
     );
   }
@@ -20,6 +35,7 @@ ProductItem.propTypes = {
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
 };
 
