f01569fb8046b80689d209d37f8c2c90b029d147
Qu4k3r <neves0007@gmail.com>
2021-03-14 21:15:15 -0300

    Requisito 10 | Adicionados botoes no carrinho (aumenta, diminui quantidade e remove produto)

--- src/components/ItemsCart.jsx
+++ src/components/ItemsCart.jsx
@@ -8,17 +8,49 @@ class ItemsCart extends Component {
     this.state = {
       quantity: 1,
     };
+    this.increaseQuantity = this.increaseQuantity.bind(this);
+    this.decreaseQuantity = this.decreaseQuantity.bind(this);
+  }
+
+  increaseQuantity() {
+    this.setState((previousState) => ({ quantity: previousState.quantity + 1 }));
+  }
+
+  decreaseQuantity() {
+    this.setState((previousState) => (
+      previousState.quantity !== 0 && { quantity: previousState.quantity - 1 }
+    ));
   }
 
   render() {
-    const { productInfo: { title, thumbnail, price } } = this.props;
+    const { productInfo: { title, thumbnail, price }, removeProduct } = this.props;
     const { quantity } = this.state;
     return (
       <div data-testid="product">
         <h4 data-testid="shopping-cart-product-name">{ title }</h4>
         <img src={ thumbnail } alt={ title } />
         <p>{`R$ ${price}`}</p>
+        <button
+          type="button"
+          onClick={ () => removeProduct(title) }
+        >
+          X
+        </button>
+        <button
+          data-testid="product-increase-quantity"
+          type="button"
+          onClick={ this.increaseQuantity }
+        >
+          +
+        </button>
         <span data-testid="shopping-cart-product-quantity">{ quantity }</span>
+        <button
+          data-testid="product-decrease-quantity"
+          type="button"
+          onClick={ this.decreaseQuantity }
+        >
+          -
+        </button>
       </div>
     );
   }
@@ -30,6 +62,7 @@ ItemsCart.propTypes = {
     thumbnail: PropTypes.string,
     price: PropTypes.number,
   }),
+  removeProduct: PropTypes.func.isRequired,
 };
 
 ItemsCart.defaultProps = {
