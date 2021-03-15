4a74b3bb74aea835f3b692209ecbffe1baef6d98
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-15 19:11:49 -0300

    Add button Add cart and implement handleAddCart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,6 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddCart = this.handleAddCart.bind(this);
+  }
+
+  handleAddCart() {
+    /** Source: https://stackoverflow.com/questions/40203350/getting-all-items-of-one-group-from-webstorage */
+    const { product } = this.props;
+    const cartData = {
+      items: [],
+    };
+
+    if (localStorage.getItem('cart')) {
+      cartData.items = JSON.parse(localStorage.getItem('cart')).items;
+    }
+    cartData.items.push(product);
+
+    localStorage.setItem('cart', JSON.stringify(cartData));
+  }
+
   render() {
     const { product } = this.props;
     return (
@@ -9,6 +29,7 @@ class ProductCard extends React.Component {
         <h3>{ product.title }</h3>
         <img src={ product.thumbnail } alt="Imagem do Produto" />
         <p>{ product.price }</p>
+        <button type="button" onClick={ this.handleAddCart }>Adicionar ao Carrinho</button>
       </section>
     );
   }
