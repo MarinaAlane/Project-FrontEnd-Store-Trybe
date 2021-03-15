f651a6426997641a3f24df5de8286ce79b6b80fa
BernardoFontes <bfontes.1997@gmail.com>
2021-03-15 03:19:14 -0300

    req 11,12

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Carrinho from './components/Carrinho';
 import Detalhes from './components/Detalhes';
-<<<<<<< HEAD
 import Checkout from './components/Checkout';
-=======
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
 
 class App extends Component {
   constructor(props) {
@@ -16,32 +13,13 @@ class App extends Component {
     };
 
     this.addToCart = this.addToCart.bind(this);
-<<<<<<< HEAD
-=======
-    this.removeItem = this.removeItem.bind(this);
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
   }
 
   addToCart(product) {
     const { cartItems } = this.state;
-<<<<<<< HEAD
     this.setState({
       cartItems: [...cartItems, product],
     });
-=======
-    if (cartItems.length > 0) {
-      return cartItems.every(({ id }) => id !== product.id)
-        && this.setState({ cartItems: [...cartItems, product] });
-    }
-    this.setState({ cartItems: [...cartItems, product] });
-  }
-
-  removeItem(itemTitle) {
-    const { cartItems } = this.state;
-    const item = cartItems.find(({ title }) => title === itemTitle);
-    cartItems.splice(cartItems.indexOf(item), 1);
-    this.setState({ cartItems });
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
   }
 
   render() {
@@ -52,18 +30,11 @@ class App extends Component {
           <Switch>
             <Route
               path="/carrinho"
-<<<<<<< HEAD
               render={ () => <Carrinho products={ cartItems } /> }
             />
             <Route
               path="/checkout"
               render={ () => <Checkout products={ cartItems } /> }
-=======
-              render={ () => (<Carrinho
-                products={ cartItems }
-                removeProduct={ this.removeItem }
-              />) }
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
             />
             <Route
               path="/:id/detalhes"
--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 import { Link } from 'react-router-dom';
-=======
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
 import ItemsCart from './ItemsCart';
 
 class Carrinho extends React.Component {
   render() {
-<<<<<<< HEAD
     const { products } = this.props;
-=======
-    const { products, removeProduct } = this.props;
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
     if (products.length > 0) {
       return (
         <section>
           { products.map((item) => (
-<<<<<<< HEAD
             <ItemsCart key={ item.id } productInfo={ item } />
           ))}
           <div>
@@ -26,14 +18,6 @@ class Carrinho extends React.Component {
             { products.reduce((total, item) => total + (item.price), 0)}
           </div>
           <Link to="/checkout" data-testid="checkout-products"> Finalizar Compra </Link>
-=======
-            <ItemsCart
-              key={ item.id }
-              productInfo={ item }
-              removeProduct={ removeProduct }
-            />
-          ))}
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
         </section>
       );
     }
@@ -47,10 +31,6 @@ class Carrinho extends React.Component {
 
 Carrinho.propTypes = {
   products: PropTypes.arrayOf(PropTypes.object),
-<<<<<<< HEAD
-=======
-  removeProduct: PropTypes.func.isRequired,
->>>>>>> 0889567ab420733feb2f20a4be759fb6a4f57e0d
 };
 
 Carrinho.defaultProps = {
