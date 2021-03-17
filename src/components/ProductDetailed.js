2df7c92950b313bf653ab7c43898b308dc7ad6a6
BBrolesi <brunohsbrolesi@gmail.com>
2021-03-16 22:03:20 -0300

    Pass product through Link

--- src/components/ProductDetailed.js
+++ src/components/ProductDetailed.js
@@ -3,30 +3,11 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ButtonCart from './shopping_cart/ButtonCart';
 import ArrowBack from './shopping_cart/logo_arrow_back.svg';
-import * as marketAPI from '../services/api';
 
 class ProductDetailed extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: [],
-    };
-
-    this.getProductFromAPI = this.getProductFromAPI.bind(this);
-  }
-
-  componentDidMount() {
-    this.getProductFromAPI();
-  }
-
-  getProductFromAPI() {
-    const recebido = this.props;
-    const { id } = recebido.match.params;
-    marketAPI.getProductFromID(id).then((product) => this.setState({ product }));
-  }
-
   render() {
-    const { product } = this.state;
+    const { location } = this.props;
+    const { product } = location.state;
     return (
       <section>
         <Link to="/">
