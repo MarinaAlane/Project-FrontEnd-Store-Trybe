2c659519579ac02e530d419c95bfe0d7b62a67a0
Raquel Picanço <picanco.raquel@gmail.com>
2021-03-17 11:46:19 -0400

    Refactor ProductDetails

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { shape, string, arrayOf, number, oneOfType } from 'prop-types';
 import Button from '../../components/Button';
 import InputContext from '../../components/InputContext';
-import ReviewForm from '../../components/ReviewForm/ReviewForm';
-import ReviewsBoard from '../../components/ReviewsBoard';
 import IncrementDecrementButton from '../../components/IncrementDecrementButton';
+import ProductDescription from '../../components/ProductDescription';
+import ReviewsSection from '../../components/ReviewsSection/ReviewsSection';
 
 export default class ProductDetails extends Component {
   constructor(props) {
@@ -27,13 +27,15 @@ export default class ProductDetails extends Component {
   }
 
   render() {
-    const { title, thumbnail, price, attributes, id, availableQuantity, quantityItems } = this.state;
+    const {
+      title, thumbnail, price, attributes, id,
+      availableQuantity, quantityItems } = this.state;
     const info = { title, price, thumbnail, attributes, id, availableQuantity };
 
     return (
       <InputContext.Consumer>
         {
-          ({ addProductToCart, reviews, saveNewReview }) => (
+          ({ addProductToCart }) => (
             <>
               <h2 data-testid="product-detail-name">{title}</h2>
               <h3>{this.formatedPrice(price)}</h3>
@@ -48,30 +50,16 @@ export default class ProductDetails extends Component {
                 submit={ false }
                 id="product-detail-add-to-cart"
                 onHandleClick={ () => (
-                  addProductToCart(...Array.from({ length: quantityItems }, () => ({ ...info })))
+                  addProductToCart(
+                    ...Array.from({ length: quantityItems },
+                      () => ({ ...info })),
+                  )
                 ) }
               >
                 Adicionar ao carrinho
               </Button>
-              <section>
-                <h4>Descrição</h4>
-                <ul>
-                  { attributes.map(({ name, value_name: value }) => (
-                    <li key={ name }>
-                      <strong>{`${name}: `}</strong>
-                      { value}
-                    </li>
-                  )) }
-                </ul>
-              </section>
-              <section>
-                <h4>Avaliações</h4>
-                <ReviewForm id={ id } onSubmitForm={ saveNewReview } />
-                <ReviewsBoard
-                  reviews={ reviews
-                    .find(({ id: reviewId }) => id === reviewId) || { reviews: [] } }
-                />
-              </section>
+              <ProductDescription attributes={ attributes } />
+              <ReviewsSection id={ id } />
             </>
           )
         }
