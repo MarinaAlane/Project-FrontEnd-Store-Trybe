9e80c8cd4878f5cd35efb9cb8e32d0da479347be
Thamirys <thamirysgoncalves.prog@gmail.com>
2021-03-18 19:26:46 -0300

    realizado requisito 09

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,22 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ButtonAdd from '../components/ButtonAdd';
 
 class ProductDetails extends React.Component {
+  formStructure() {
+    return (
+      <form>
+        <div>
+          <input type="email" placeholder="E-mail" required />
+          <select required>
+            <option value="5">
+              5
+            </option>
+            <option value="4">
+              4
+            </option>
+            <option value="3">
+              3
+            </option>
+            <option value="2">
+              2
+            </option>
+            <option value="1">
+              1
+            </option>
+          </select>
+        </div>
+        <div>
+          <textarea rows="5" cols="30" data-testid="product-detail-evaluation" />
+        </div>
+      </form>
+    );
+  }
+
   render() {
     const { location } = this.props;
     const params = new URLSearchParams(location.search);
-    const title = params.get('title');
-    const image = params.get('image');
-    const price = params.get('price');
+    const product = {
+      title: params.get('title'),
+      image: params.get('image'),
+      price: params.get('price'),
+    };
     return (
       <div>
-        <h2 data-testid="product-detail-name">{title}</h2>
-        <img src={ image } alt={ title } />
+        <h2 data-testid="product-detail-name">{product.title}</h2>
+        <img src={ product.image } alt={ product.title } />
         <h4>
           Preço: $
           {' '}
-          {price}
+          {product.price}
         </h4>
+        <ButtonAdd
+          product={ product }
+          id="product-detail-add-to-cart"
+          idLink="shopping-cart-button"
+        />
+        {this.formStructure()}
       </div>
     );
   }
