41881146b4463fd68b94b5d94553ff95a3b99c6e
Fernando Resende <fernandocastellob@gmail.com>
2021-03-18 15:36:03 -0300

    requisito 11 ok

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -2,6 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductDetails extends React.Component {
+  formStructure() {
+    return (
+      <form>
+        <div>
+          <input type="email" placeholder="E-mail" required />
+          <select required>
+            <option value="5">
+              5
+            </option>
+            <option value="4">
+              4
+            </option>
+            <option value="3">
+              3
+            </option>
+            <option value="2">
+              2
+            </option>
+            <option value="1">
+              1
+            </option>
+          </select>
+        </div>
+        <div>
+          <textarea rows="5" cols="30" data-testid="product-detail-evaluation" />
+        </div>
+      </form>
+    );
+  }
   render() {
     const { location } = this.props;
     const params = new URLSearchParams(location.search);
@@ -16,8 +45,10 @@ class ProductDetails extends React.Component {
           Preço: $
           {' '}
           {price}
+          {this.formStructure()}
         </h4>
       </div>
+
     );
   }
 }
