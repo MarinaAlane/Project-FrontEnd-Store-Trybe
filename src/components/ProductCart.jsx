dd4602f47a7fb0f9c96b2c73ccaae9540b215749
Pablo Iago <evangelista.pabloiago@gmail.com>
2021-03-18 20:51:23 -0500

    FEAT-requisito10

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -2,13 +2,44 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 class ProductCart extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    const { eachProduct } = this.props;
+    const { quantity } = eachProduct;
+    this.state = {
+      quantity,
+    }
+    this.productDecrease = this.productDecrease.bind(this)
+    this.productIncrease = this.productIncrease.bind(this)
+  }
+
+  productIncrease() {
+    this.setState(previousQuantity =>({
+      quantity: previousQuantity.quantity + 1
+    }))
+  }
+
+  productDecrease() {
+    this.setState(({quantity}) =>({
+        quantity:(quantity > 0) ? quantity - 1 : 0,
+    }));
+  }
+
   render() {
     const { eachProduct } = this.props;
-    const { title, price, quantity, thumbnail } = eachProduct;
+    const { title, price, thumbnail } = eachProduct;
+    const { quantity } = this.state;
     return (
       <div>
         <h1 data-testid="shopping-cart-product-name">{ title }</h1>
-        <p>{ price }</p>
+        <p>
+          <button data-testid="product-increase-quantity" onClick={ this.productIncrease }>+</button>
+          <button data-testid="product-decrease-quantity" onClick={ this.productDecrease }>-</button>
+          <button>X</button>
+          
+          { price }
+        </p>
         <p data-testid="shopping-cart-product-quantity">{ quantity }</p>
         <img src={ thumbnail } alt={ title } />
         <hr />
