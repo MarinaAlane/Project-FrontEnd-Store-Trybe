1230d30d378b633a88d63c66f8c4f53f79ce8cdc
Nathália <nathaliaveneziano@icloud.com>
2021-03-19 13:56:43 -0300

    FEAT-requisito 11

--- src/pages/DetailedProduct.jsx
+++ src/pages/DetailedProduct.jsx
@@ -9,6 +9,7 @@ class DetailedProduct extends React.Component {
     const { state: { array } } = location;
     console.log(this.props, location);
     const { title, thumbnail, price } = array;
+
     return (
       <div>
         <header>
@@ -20,16 +21,35 @@ class DetailedProduct extends React.Component {
           <h2 data-testid="product-detail-name">
             { title }
           </h2>
-          <img src={ thumbnail } alt={ title } />
-          <p>{ price }</p>
+          <div className="product-detail-content">
+            <img src={ thumbnail } alt={ title } />
+            <p>{ price }</p>
+            <button
+              type="button"
+              data-testid="product-detail-add-to-cart"
+              onClick={ () => saveProduct(array) }
+            >
+              Adicionar ao carrinho
+            </button>
+          </div>
+          <div className="product-detail-rating">
+            <h2>Avaliações</h2>
+            <div className="product-detail-form">
+              <p>
+              <input type="email" placeholder="Email" />
+              <input type="radio" name="star" id=""/>1
+              <input type="radio" name="star" id=""/>2
+              <input type="radio" name="star" id=""/>3
+              <input type="radio" name="star" id=""/>4
+              <input type="radio" name="star" id=""/>5
+              </p>
+              <p>
+                <textarea data-testid="product-detail-evaluation" cols="30" rows="10"></textarea>
+              </p>
+              <button type="button">Avaliar</button>
+            </div>
+          </div>
         </main>
-        <button
-          type="button"
-          data-testid="product-detail-add-to-cart"
-          onClick={ () => saveProduct(array) }
-        >
-          Adicionar ao carrinho
-        </button>
       </div>
     );
   }
