66fddf72727c891833b56d33b35ecc0b13d9d81f
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-03-19 19:07:29 -0300

    css main features

---
+++ src/components/CategoriesList.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import * as api from '../services/api';
+
+class CategoriesList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categoryName: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then(
+      (response) => this.setState({ categoryName: response }),
+    );
+  }
+
+  render() {
+    const { activateButton } = this.props;
+    const { categoryName } = this.state;
+    return (
+      <aside>
+        {categoryName.map((currentValue) => (
+          <div className="category-box" key={ currentValue.id }>
+            <input
+              key={ currentValue.id }
+              type="radio"
+              data-testid="category"
+              onClick={ activateButton }
+              className={ currentValue.id }
+              name={ currentValue }
+              id={ currentValue }
+            />
+            <label for={currentValue.id}>
+              {currentValue.name}
+            </label>
+          </div>
+        )) }
+      </aside>
+    );
+  }
+}
+
+CategoriesList.propTypes = {
+  activateButton: PropTypes.func,
+}.isRequired;
+
+export default CategoriesList;
