cd3ecb4649fd80b65784cb39df357f4ff3477330
Miguel Dantas <miiguelalb@gmail.com>
2021-03-23 00:47:17 -0300

    REQ 8 Conclúido

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -5,11 +5,11 @@ import { saveToCart } from '../services/cart';
 
 class AddToCart extends React.Component {
   render() {
-    const { productInfos: { id, title, amount, testId } } = this.props;
+    const { productInfos: { id, title, amount } } = this.props;
     return (
       <button
         type="button"
-        data-testid={ testId }
+        data-testid="product-add-to-cart"
         onClick={ () => saveToCart(id, title, amount) }
       >
         Adicionar ao Carrinho
@@ -23,7 +23,7 @@ AddToCart.propTypes = {
     id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     amount: PropTypes.number.isRequired,
-    testId: PropTypes.string.isRequired,
   }).isRequired,
 };
+
 export default AddToCart;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { recoverCart } from '../services/cart';
 
 class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartItems: recoverCart(),
+    };
+  }
+
   render() {
+    const { cartItems } = this.state;
+    console.log(cartItems);
     return (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      <div>
+        {cartItems
+          ? (cartItems.map(({ id, name, amount }) => (
+            <div className="cartItem" key={ id }>
+              <p data-testid="shopping-cart-product-name">{ name }</p>
+              <p data-testid="shopping-cart-product-quantity">{ amount }</p>
+            </div>
+          )))
+          : (
+            <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+          )}
+      </div>
     );
   }
 }
