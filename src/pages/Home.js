b97bb40df2398ffc22bb4d75f1dbba4ccf7e35c5
negres <nataliaknegres@gmail.com>
2021-05-07 00:06:27 -0300

    Refatorando código p/1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,7 +55,7 @@ class Home extends React.Component {
     this.setState({
       productsList: response.results,
     });
-    console.log(response);
+    console.log(response.results);
   }
 
   async getCategoriesList() {
